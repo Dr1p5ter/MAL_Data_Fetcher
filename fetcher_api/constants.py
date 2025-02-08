7e0d72258935f3c7685b084335bec55e85dd9509
Matthew McLaren <Wildcatdud@gmail.com>
2025-02-08 02:19:31 -0500

    Update constants.py

--- fetcher_api/constants.py
+++ fetcher_api/constants.py
@@ -1,9 +1,10 @@
 # Networking constants
 
-MAL_BASE : str = 'https://myanimelist.net'
-MAL_OAUTH2_ENDPOINT : str = MAL_BASE + '/v1/oauth2'
-MAL_ANIME_ENDPOINT : str = MAL_BASE + '/v2/anime/'
-MAL_MANGA_ENDPOINT : str = MAL_BASE + '/v2/manga/'
+MAL_OAUTH2_BASE : str = 'https://myanimelist.net'
+MAL_OAUTH2_ENDPOINT : str = MAL_OAUTH2_BASE + '/v1/oauth2'
+MAL_BASE : str = 'https://api.myanimelist.net'
+MAL_ANIME_ENDPOINT : str = MAL_BASE + '/v2/anime'
+MAL_MANGA_ENDPOINT : str = MAL_BASE + '/v2/manga'
 
 # Organization constants
 
@@ -23,11 +24,15 @@ API_CLIENT_ID_LEN : int = 32
 API_CLIENT_SECRET_LEN : int = 64
 
 # MAL_classes constants
-
-ANIMELIST_FIELDS = [
-    'id', 'title', 'main_picture', 'alternative_titles', 'start_date',
-    'end_date', 'synopsis', 'mean', 'rank', 'popularity', 'num_list_users',
-    'num_scoring_users', 'nsfw', 'genres', 'created_at', 'updated_at',
-    'media_type', 'status', 'start_season', 'broadcast', 'source',
-    'average_episode_duration', 'rating', 'studios'
+ANIMELISTNODE_DEFAULT_FIELDS = [
+    'id', 'title', 'main_picture'
 ]
+ANIMELISTNODE_OPTIONAL_FIELDS = [
+    'alternative_titles', 'start_date', 'end_date', 'synopsis', 'mean', 'rank',
+    'popularity', 'num_list_users', 'num_scoring_users', 'nsfw', 'genres',
+    'created_at', 'updated_at', 'media_type', 'status', 'num_episodes', 
+    'start_season', 'broadcast', 'source', 'average_episode_duration',
+    'rating', 'studios'
+]
+
+ANIMELISTNODE_FIELDS = ANIMELISTNODE_DEFAULT_FIELDS + ANIMELISTNODE_OPTIONAL_FIELDS
