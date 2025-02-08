d0ad9902c67686ecf06395426e3cd64441824fad
Matthew McLaren <Wildcatdud@gmail.com>
2025-02-08 02:20:00 -0500

    Update main.py
    changed some of the unofficial crud testing more to come later

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
 # native imports
 
-from json import dumps
 from sys import exit
 from traceback import format_exc
+from pprint import pprint
 
 # local imports
-from fetcher_api.authorise import *
-from fetcher_api.key import *
+from fetcher_api.token import APIToken
+from fetcher_api.key import APIKey
+from fetcher_api.MAL_classes import *
 
 if __name__ == '__main__' :
     # probe the user for API credentials if there have been none provided before runtime
-    makeKeyFile()
-
-    # grab API information
     try :
-        data_pair : dict = grabAPIInfo()
-        client_id = data_pair['_id']
-        client_secret = data_pair['_secret']
-    except Exception as UnhandledException :
-        print(format_exc())
+        key : APIKey = APIKey()
+    except :
+        pprint(format_exc())
         exit(-1)
+    # pprint(key)
 
     # attempt to load a token if one exists
     try :
-        token = loadTokenData()
-        validateToken(token['access_token'])
-    except (TokenFileNotFoundError, TokenValidationError) :
-        token = getAPIToken(client_id, client_secret)
-    except Exception as UnhandledException:
-        print(format_exc())
+        token : APIToken = APIToken(key.getKey())
+    except :
+        pprint(format_exc())
         exit(-1)
+    # pprint(token)
 
-    try :
-        url : str = 'https://api.myanimelist.net/v2/anime?offset=50&q=one&limit=50'
-        response : Response = get(url, headers={'Authorization' : f'Bearer {token['access_token']}'})
-        response.raise_for_status()
-        data = response.json()
-        response.close()
-    except HTTPError :
-        if response.status_code == 401 :
-            token = refreshAPIToken(client_id, client_secret)
-    print(dumps(data, indent=4))
+    # example query
+    animelist = AnimeList(token.getToken()['access_token'],
+                          'Neon',
+                          limit=1,
+                          optional_fields=ANIMELISTNODE_OPTIONAL_FIELDS)
+
+    for node in animelist.data :
+        pprint(node)
+    # pprint(animelist.paging)
+
     exit(1)
