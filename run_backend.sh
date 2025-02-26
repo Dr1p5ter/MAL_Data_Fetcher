1ff7738a8355f6f58f66b11768f38a514221ce88
Matthew McLaren <Wildcatdud@gmail.com>
2025-02-26 15:35:52 -0500

    Force additional checks

--- run_backend.sh
+++ run_backend.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# download required modules
+python3 -m pip install -r requirements.txt
+
+# run app
 python3 -m flask_backend.app
--- run_frontend.sh
+++ run_frontend.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# go into the frontend directory
 cd react_frontend
+
+# check to see if the packages need installed
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
+
+# run the frontend
 npm run dev
