0adea4d3182ecbc85e392a06545c281dda6f1ddf
Matthew McLaren <Wildcatdud@gmail.com>
2025-03-17 17:17:48 -0400

    Clean project as blank slate
    I will work on this once I iron out backend design!

--- react_frontend/src/main.jsx
+++ react_frontend/src/main.jsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
-
+    <StrictMode>
+        <BrowserRouter>
+			<Routes>
+				<Route index element={
+					<>
+						<div style={{ textAlign: 'center', marginTop: '20px' }}>
+							<h1>Hello World :D</h1>
+							<h2>This app unfortunately has no UI/UX design yet!</h2>
+							<h2>Please come back momentarily!!</h2>
+						</div>
+					</>
+				}/>
+			</Routes>
+        </BrowserRouter>
+    </StrictMode>
 )
